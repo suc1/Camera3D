4c043e9fcc1e18182fdc740ace224811e864efb3
cheng <su.cheng@ledsmart.com>
2022-08-24 14:16:55 -0700

    Finish Camera3D, but no test

--- camera/camera_3d.cpp
+++ camera/camera_3d.cpp
@@ -28,27 +28,88 @@ const glm::mat4& Camera3D::GetProjectionMatrix(float size_w, float size_h) {
   return projection_mat_;
 }
 
+//由空间一点pos，求出在图象平面的坐标(u,v)
 const std::pair<float, float> Camera3D::GetUVAtPos(const glm::vec3& pos) {
-  // TODO
+  auto posUV = mvp_mat_ * glm::vec4(pos, 1.0f);
+  if(posUV.w<0.0f) return {1.1f, 1.1f};
 
-  return std::make_pair(0.0f, 0.0f);
+  posUV /= posUV.w;
+  if(posUV.x < -1.0f || posUV.x > 1.0f || posUV.y < -1.0f || posUV.y > 1.0f) return {1.1f, 1.1f};
+
+
+  return std::make_pair(posUV.x, posUV.y);
 }
 
 void Camera3D::ProcessKeyboard(CameraMovement direction, float delta_time) {
-  // TODO
+  float velocity = movement_speed_ * delta_time;
+
+  switch(direction) {
+    case CameraMovement::FORWARD:
+      position_ += front_ * velocity;
+      break;
+    case CameraMovement::BACKWARD:
+      position_ -= front_ * velocity;
+      break;
+
+    case CameraMovement::LEFT:
+      position_ += right_ * velocity;
+      break;
+    case CameraMovement::RIGHT:
+      position_ -= right_ * velocity;
+      break;
+
+    case CameraMovement::DOWN:
+      position_ -= up_ * velocity;
+      break;
+    case CameraMovement::UP:
+      position_ += up_ * velocity;
+      break;
+
+    default:
+      break;
+  }
+
+  UpdateViewMatrix();
+  UpdateMVPMatrix();
 }
 
 void Camera3D::ProcessMouseMovement(float x_offset, float y_offset) {
-  // TODO
+  x_offset *= sensitivity_;
+  y_offset *= sensitivity_;
+
+  roll_ += x_offset;
+  roll_ = std::fmod(roll_, 360);
+
+  pitch_ += y_offset;
+  pitch_ = std::fmod(pitch_, 180);
+
+  UpdateViewMatrix();
+  UpdateMVPMatrix();
 }
 
 // Calculates the front vector from the Camera's (updated) Euler Angles
 void Camera3D::UpdateViewMatrix() {
-  // TODO
+  glm::mat4 rotate_x = glm::rotate(glm::mat4(1.0f), glm::radians(pitch_), glm::vec3(1.0f, 0.0f, 0.0f));
+  glm::mat4 rotate_y = glm::rotate(glm::mat4(1.0f), glm::radians(yaw_),   glm::vec3(0.0f, 1.0f, 0.0f));
+  glm::mat4 rotate_z = glm::rotate(glm::mat4(1.0f), glm::radians(roll_),  glm::vec3(0.0f, 0.0f, 1.0f));
+
+  glm::mat4 translate = glm::translate(glm::mat4(1.0f), position_);
+
+  glm::mat4 model = translate * rotate_z * rotate_y * rotate_x;
+
+  right_ = glm::vec3(model[0][0], model[0][1], model[0][2]);
+  up_    = glm::vec3(model[1][0], model[1][1], model[1][2]);
+  front_ = glm::vec3(-model[2][0], -model[2][1], -model[2][2]);
+
+  view_mat_ = glm::inverse(model);
 }
 
 void Camera3D::UpdateProjectionMatrix(float size_w, float size_h) {
-  // TODO
+  if (size_w == size_w_ && size_h == size_h_) return;
+
+  size_w_ = size_w;
+  size_h_ = size_h;
+  projection_mat_ = glm::perspective(static_cast<float>(M_PI_4), size_w_/size_h_, 0.1f, 1000.0f);
 }
 
 void Camera3D::UpdateMVPMatrix(float size_w, float size_h) {
